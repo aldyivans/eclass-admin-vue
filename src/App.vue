449b5a1f63b837c5268da4197e414a509ec48f68
aldy ivan <ivanbrutalart@gmail.com>
2018-10-22 18:36:28 +0700

    admin template

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,15 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="container-fluid">
+    <div class="row">
+      <div style="font-weight: bold;" class="m-0 p-0 border-bottom col-lg-12 d-flex align-items-center">
+          <div class="m-0 p-0">
+              <a class="nav-link mb-0 pb-0" href="index.html"><h1>EClass</h1></a>
+          </div>
+          <div class="col m-0 text-right">
+              <a href="#" style="font-weight: bold; color: black;">Log Out</a>
+          </div>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
