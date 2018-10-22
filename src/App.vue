21a99f3786305b00dbf47ea8a498759ece71c2a9
aldy ivan <ivanbrutalart@gmail.com>
2018-10-23 01:31:26 +0700

    template master fix

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
-  <div id="app" class="container-fluid">
-    <div class="row">
-      <div style="font-weight: bold;" class="m-0 p-0 border-bottom col-lg-12 d-flex align-items-center">
-          <div class="m-0 p-0">
-              <a class="nav-link mb-0 pb-0" href="index.html"><h1>EClass</h1></a>
-          </div>
-          <div class="col m-0 text-right">
-              <a href="#" style="font-weight: bold; color: black;">Log Out</a>
-          </div>
-      </div>
-      <div class="col-12 col-sm-3 p-0 bg-primary">
-        <div class="list-group" id="list-tab" role="tablist">
-          <a style="font-size: 25px;" class="list-group-item list-group-item-action border-0 bg-primary text-white rounded-0" id="list-Upload-list" data-toggle="list" href="#list-home" role="tab"><h5>Upload Course</h5></a>
-          <a style="font-size: 25px;" class="list-group-item list-group-item-action border-0 bg-primary text-white rounded-0" id="list-Catrgories-list" data-toggle="list" href="#list-profile" role="tab"><h5>Categories</h5></a>
-          <a style="font-size: 25px;"  class="list-group-item list-group-item-action border-0 bg-primary text-white rounded-0" id="list-User-list" data-toggle="list" href="list-User" role="tab"><h5>User</h5></a>
+  <div id="app">
+    <div class="container-fluid" >
+      <div class="row">
+        <div style="font-weight: bold;" class="m-0 p-0 border-bottom col-lg-12 d-flex align-items-center" v-if="this.isLoggedIn">
+            <div class="px-3 m-0">
+                <router-link to="/"><h1 class="my-2">EClass</h1></router-link>
+            </div>
+            <div class="col m-0 text-right">
+                <button type="button" class="border-0 bg-white font-weight-bold">Log Out</button>
+            </div>
         </div>
+        <div class="col-12 col-sm-2 p-0 bg-primary" v-if="this.isLoggedIn">
+          <div class="list-group mt-3" id="list-tab" role="tablist">
+            <router-link to="/" style="font-size: 25px;" class="list-group-item list-group-item-action border-0 bg-primary text-white rounded-0"><h5 class="m-0 font-weight-bold">Upload Course</h5></router-link>
+            <router-link to="/"  style="font-size: 25px;" class="list-group-item list-group-item-action border-0 bg-primary text-white rounded-0"><h5 class="m-0 font-weight-bold">Categories</h5></router-link>
+            <router-link to="/"  style="font-size: 25px;"  class="list-group-item list-group-item-action border-0 bg-primary text-white rounded-0"><h5 class="m-0 font-weight-bold">User</h5></router-link>
+          </div>
+        </div>
+        <router-view/>
       </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
