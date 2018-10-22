a67346a3b1b71e17dfaca69451e68232f85c61c8
aldy ivan <ivanbrutalart@gmail.com>
2018-10-22 23:01:04 +0700

    admin template fix

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,27 @@
               <a href="#" style="font-weight: bold; color: black;">Log Out</a>
           </div>
       </div>
+      <div class="col-12 col-sm-3 p-0 bg-primary">
+        <div class="list-group" id="list-tab" role="tablist">
+          <a style="font-size: 25px;" class="list-group-item list-group-item-action border-0 bg-primary text-white rounded-0" id="list-Upload-list" data-toggle="list" href="#list-home" role="tab"><h5>Upload Course</h5></a>
+          <a style="font-size: 25px;" class="list-group-item list-group-item-action border-0 bg-primary text-white rounded-0" id="list-Catrgories-list" data-toggle="list" href="#list-profile" role="tab"><h5>Categories</h5></a>
+          <a style="font-size: 25px;"  class="list-group-item list-group-item-action border-0 bg-primary text-white rounded-0" id="list-User-list" data-toggle="list" href="list-User" role="tab"><h5>User</h5></a>
+        </div>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+  
+  export default {
+    name: 'app',
+    data() {
+      return {
+        isLoggedIn: false
+      }
+    }
+  }
+
+</script>
